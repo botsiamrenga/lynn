14dcedd7541fdda60b8a1b696e5bbb6c5aec00b0
didikteatochhawng <81432899+didikteatochhawng@users.noreply.github.com>
2021-06-11 00:03:55 +0530

    Update __nightmode.py

--- LaylaRobot/modules/__nightmode.py
+++ LaylaRobot/modules/__nightmode.py
@@ -168,7 +168,7 @@ scheduler.start()
 
 
 __help__ = f"""
- ❍ /nightmode on/off
+ ➤ /nightmode on/off
  
 **Note:** Night Mode chats get Automatically closed at 12pm(IST)
 and Automatically openned at 6am(IST) To Prevent Night Spams.
