2f72d6649817582c5624db1508bdb6dee068ce9f
Didiktea <86914002+Didiktea9@users.noreply.github.com>
2021-08-04 16:11:02 +0530

    Update _pyrosongs.py

--- LaylaRobot/modules/_pyrosongs.py
+++ LaylaRobot/modules/_pyrosongs.py
@@ -28,7 +28,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('🔎 ♪Finding the song... /n/n🅻︎🅾︎🅰︎🅳︎🅸︎🅽︎🅶︎➪')
+    m = message.reply('🔎 ♪Finding the song...')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
