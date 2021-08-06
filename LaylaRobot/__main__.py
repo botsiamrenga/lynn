ef08cb8ea655ba9a70005ba128f71de0322f76fe
Didiktea <86914002+Didiktea9@users.noreply.github.com>
2021-08-06 13:08:10 +0530

    Update __main__.py

--- LaylaRobot/__main__.py
+++ LaylaRobot/__main__.py
@@ -220,7 +220,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>".format(
+            "𝙻𝚢𝚗𝚗 𝚒𝚜 𝚊𝚕𝚒𝚟𝚎!\n<b>Made by @Didiktea\n<b>Join support Group @lynnsupportgroup".format(
                 uptime
             ),
             parse_mode=ParseMode.HTML,
