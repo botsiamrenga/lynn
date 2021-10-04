a23fbe16fe5055e749da5ddd6c69a1c865047912
Didiktea <86914002+Didiktea9@users.noreply.github.com>
2021-10-04 07:55:52 +0530

    Update userinfo.py

--- LaylaRobot/modules/userinfo.py
+++ LaylaRobot/modules/userinfo.py
@@ -290,7 +290,7 @@ def info(update: Update, context: CallbackContext):
         text += "\n\nThe Disaster level of this person is 'God'."
         disaster_level_present = True
     elif user.id in DEV_USERS:
-        text += "\n\nThis user is member of 'Hero Association'."
+        text += "\n\nThis user is member of 'Mizo Android Users'."
         disaster_level_present = True
     elif user.id in DRAGONS:
         text += "\n\nThe Disaster level of this person is 'Dragon'."
@@ -306,7 +306,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/OnePunchUpdates/155">?</a>]'.format(
+        text += ' [<a href="https://t.me/puituflynn">?</a>]'.format(
             bot.username
         )
 
