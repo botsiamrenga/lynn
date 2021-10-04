e6ddff252efdc3d91c35ca3464ec5d15fb9eeb93
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-10-04 17:25:58 +0530

    Update mongo.py

--- LaylaRobot/mongo.py
+++ LaylaRobot/mongo.py
@@ -1,10 +1,10 @@
 import asyncio
 import sys
 
-from motor import motor_asyncio
+
 from LaylaRobot import MONGO_DB_URI 
 from pymongo import MongoClient
-from pymongo.errors import ServerSelectionTimeoutError
+
 from LaylaRobot.conf import get_int_key, get_str_key
 
 
@@ -15,10 +15,4 @@ MONGO_DB = "DaisyX"
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
-db = motor[MONGO_DB]
 db = client["LaylaRobot"]
-try:
-    asyncio.get_event_loop().run_until_complete(motor.server_info())
-except ServerSelectionTimeoutError:
-    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
