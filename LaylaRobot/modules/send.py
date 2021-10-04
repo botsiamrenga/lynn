fe6bf561a964eccbaaa699d691e5ca6e1b8a8b45
Didiktea <86914002+Didiktea9@users.noreply.github.com>
2021-10-04 12:51:04 +0530

    Update send.py

--- LaylaRobot/modules/send.py
+++ LaylaRobot/modules/send.py
@@ -18,3 +18,24 @@ ADD_CCHAT_HANDLER = DisableAbleCommandHandler("snd", send)
 dispatcher.add_handler(ADD_CCHAT_HANDLER)
 __command_list__ = ["snd"]
 __handlers__ = [ADD_CCHAT_HANDLER]
+
+
+# Stats Module
+
+
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - bot_start_time)
+    cpu = psutil.cpu_percent()
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    process = psutil.Process(os.getpid())
+    stats = f"""
+{USERBOT_USERNAME}
+------------------
+UPTIME: {formatter.get_readable_time((bot_uptime))}
+BOT: {round(process.memory_info()[0] / 1024 ** 2)} MB
+CPU: {cpu}%
+RAM: {mem}%
+DISK: {disk}%
+"""
+    return stats
