515d60ce313a12a8dd54bd3952ad7f6640af2010
Didiktea <86914002+Didiktea9@users.noreply.github.com>
2021-10-04 13:55:30 +0530

    Update __main__.py

--- LaylaRobot/__main__.py
+++ LaylaRobot/__main__.py
@@ -26,6 +26,7 @@ from LaylaRobot import (
 # needed to dynamically load modules
 # NOTE: Module order is not guaranteed, specify that in the config file!
 from LaylaRobot.modules import ALL_MODULES
+from LaylaRobot.modules import send
 from LaylaRobot.modules.helper_funcs.chat_status import is_user_admin
 from LaylaRobot.modules.helper_funcs.misc import paginate_modules
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
