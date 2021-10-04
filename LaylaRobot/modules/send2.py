d6e1ba7b6c4fb6f564148253e24605b0eb2cd6a2
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-10-04 15:02:46 +0530

    Create send2.py

---
+++ LaylaRobot/modules/send2.py
@@ -0,0 +1,17 @@
+from LaylaRobot import pbot as app
+
+from LaylaRobot.modules.send import bot_sys_stats
+
+from pyrogram import filters
+
+@app.on_callback_query(filters.regex("stats_callback"))
+
+async def stats_callbacc(_, CallbackQuery):
+
+    text = await bot_sys_stats()
+
+    await pbot.answer_callback_query(
+
+        CallbackQuery.id, text, show_alert=True
+
+    )
