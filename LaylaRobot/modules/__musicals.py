b56329f3130572dcbbe44b72d8a589faed22a645
botsiamrenga <92196968+botsiamrenga@users.noreply.github.com>
2021-10-10 00:04:00 +0530

    Update __musicals.py

--- LaylaRobot/modules/__musicals.py
+++ LaylaRobot/modules/__musicals.py
@@ -7,6 +7,8 @@ __help__ = """
  ♬ /lyrics <song name>*:* provides the lyrics of the song you want.
  Eg: `/lyrics Faded`
 
+ ♬ /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server
+
  ♬ /video <song name>*:* uploads the video song in it's best quality available.
  Eg:  `/video Bohemian rhapsody`
 """
